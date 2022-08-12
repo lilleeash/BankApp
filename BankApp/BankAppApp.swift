385e3951045acfef247dd7d11a082db52504a685
Darya <lilleeash@icloud.com>
2022-08-12 15:13:59 +0300

    Initial Commit

---
+++ BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Darya Zhitova on 12.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
